372780403063c5352f0236252ed20efb6d93e901
John-David Dalton <john.david.dalton@gmail.com>
2013-12-05 21:48:07 -0800

    Allow `invert` to accept a `multiValue` argument.

--- modern/objects/invert.js
+++ modern/objects/invert.js
@@ -8,28 +8,56 @@
  */
 var keys = require('./keys');
 
+/** Used for native method references */
+var objectProto = Object.prototype;
+
+/** Native method shortcuts */
+var hasOwnProperty = objectProto.hasOwnProperty;
+
 /**
- * Creates an object composed of the inverted keys and values of the given object.
+ * Creates an object composed of the inverted keys and values of the given
+ * object. If the given object contains duplicate values, subsequent values
+ * will overwrite property assignments of previous values unless `multiValue`
+ * is `true`.
  *
  * @static
  * @memberOf _
  * @category Objects
  * @param {Object} object The object to invert.
+ * @param {boolean} [multiValue=false] Allow multiple values per key.
  * @returns {Object} Returns the created inverted object.
  * @example
  *
  * _.invert({ 'first': 'fred', 'second': 'barney' });
  * // => { 'fred': 'first', 'barney': 'second' }
+ *
+ * // without `multiValue`
+ * _.invert({ 'first': 'fred', 'second': 'barney', 'third': 'fred' });
+ * // => { 'fred': 'third', 'barney': 'second' }
+ *
+ * // with `multiValue`
+ * _.invert({ 'first': 'fred', 'second': 'barney', 'third': 'fred' }, true);
+ * // => { 'fred': ['first', 'third'], 'barney': 'second' }
  */
-function invert(object) {
+function invert(object, multiValue) {
   var index = -1,
       props = keys(object),
       length = props.length,
       result = {};
 
   while (++index < length) {
-    var key = props[index];
-    result[object[key]] = key;
+    var key = props[index],
+        value = object[key];
+
+    if (multiValue && hasOwnProperty.call(result, value)) {
+      if (typeof result[value] == 'string') {
+        result[value] = [result[value]];
+      }
+      result[value].push(key);
+    }
+    else {
+      result[value] = key;
+    }
   }
   return result;
 }
