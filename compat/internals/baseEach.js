d1459e7e3bc78d7ec368afc55a8fc3915df55c21
John-David Dalton <john.david.dalton@gmail.com>
2013-12-15 18:05:50 -0800

    Cleanup baseEach.

--- compat/internals/baseEach.js
+++ compat/internals/baseEach.js
@@ -6,23 +6,43 @@
  * Copyright 2009-2013 Jeremy Ashkenas, DocumentCloud and Investigative Reporters & Editors
  * Available under MIT license <http://lodash.com/license>
  */
-var createIterator = require('./createIterator'),
-    eachIteratorOptions = require('./eachIteratorOptions');
+var baseCreateCallback = require('./baseCreateCallback'),
+    forOwn = require('../objects/forOwn'),
+    isString = require('../objects/isString'),
+    support = require('../support');
 
 /**
- * A function compiled to iterate `arguments` objects, arrays, objects, and
- * strings consistenly across environments, executing the callback for each
- * element in the collection. The callback is bound to `thisArg` and invoked
- * with three arguments; (value, index|key, collection). Callbacks may exit
- * iteration early by explicitly returning `false`.
+ * Iterates `arguments` objects, arrays, objects, and strings consistently
+ * across environments, executing the callback for each element in the
+ * collection. The callback is bound to `thisArg` and invoked with three
+ * arguments; (value, index|key, collection). Callbacks may exit iteration
+ * early by explicitly returning `false`.
  *
  * @private
- * @type Function
  * @param {Array|Object|string} collection The collection to iterate over.
  * @param {Function} [callback=identity] The function called per iteration.
  * @param {*} [thisArg] The `this` binding of `callback`.
  * @returns {Array|Object|string} Returns `collection`.
  */
-var baseEach = createIterator(eachIteratorOptions);
+function baseEach(collection, callback, thisArg) {
+  var index = -1,
+      iterable = collection,
+      length = collection ? collection.length : 0;
+
+  callback = callback && typeof thisArg == 'undefined' ? callback : baseCreateCallback(callback, thisArg, 3);
+  if (typeof length == 'number') {
+    if (support.unindexedChars && isString(iterable)) {
+      iterable = iterable.split('');
+    }
+    while (++index < length) {
+      if (callback(iterable[index], index, collection) === false) {
+        break;
+      }
+    }
+  } else {
+    forOwn(collection, callback);
+  }
+  return collection;
+}
 
 module.exports = baseEach;
