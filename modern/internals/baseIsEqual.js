12494afa3563d7e95b690ada1cee9ed9fe2bd941
John-David Dalton <john.david.dalton@gmail.com>
2014-01-11 19:43:57 -0800

    Make `isEqual` work with plain objects containing `constructor` properties.

--- modern/internals/baseIsEqual.js
+++ modern/internals/baseIsEqual.js
@@ -116,6 +116,7 @@ function baseIsEqual(a, b, callback, isWhere, stackA, stackB) {
 
     // non `Object` object instances with different constructors are not equal
     if (ctorA != ctorB &&
+          !(hasOwnProperty.call(a, 'constructor') && hasOwnProperty.call(b, 'constructor')) &&
           !(isFunction(ctorA) && ctorA instanceof ctorA && isFunction(ctorB) && ctorB instanceof ctorB) &&
           ('constructor' in a && 'constructor' in b)
         ) {
