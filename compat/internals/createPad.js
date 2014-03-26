732b19871a7c05a9e9ed657615e423d8a9f8fa61
John-David Dalton <john.david.dalton@gmail.com>
2014-03-25 20:47:57 -0700

    Update Underscore license year. [ci skip]

--- compat/internals/createPad.js
+++ compat/internals/createPad.js
@@ -3,7 +3,7 @@
  * Build: `lodash modularize exports="node" -o ./compat/`
  * Copyright 2012-2014 The Dojo Foundation <http://dojofoundation.org/>
  * Based on Underscore.js 1.6.0 <http://underscorejs.org/LICENSE>
- * Copyright 2009-2013 Jeremy Ashkenas, DocumentCloud and Investigative Reporters & Editors
+ * Copyright 2009-2014 Jeremy Ashkenas, DocumentCloud and Investigative Reporters & Editors
  * Available under MIT license <http://lodash.com/license>
  */
 var repeat = require('../strings/repeat');
