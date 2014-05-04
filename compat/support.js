6aab54059f5d66a97623d22cf422aff2f6e5018d
John-David Dalton <john.david.dalton@gmail.com>
2014-05-04 10:04:49 -0700

    Add doc note on Firefox OS certified apps. [ci skip]

--- compat/support.js
+++ compat/support.js
@@ -93,7 +93,8 @@ var support = {};
 
   /**
    * Detect if functions can be decompiled by `Function#toString`
-   * (all but PS3 and older Opera mobile browsers; forced `false` for Windows 8 apps).
+   * (all but Firefox OS certified apps, older Opera mobile browsers, and
+   * the PlayStation 3; forced `false` for Windows 8 apps).
    *
    * @memberOf _.support
    * @type boolean
